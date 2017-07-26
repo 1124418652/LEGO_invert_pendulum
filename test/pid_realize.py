fc4302b45f596e3f8be012fbf56f9ac42e0752f2
hejun <1124418652@qq.com>
2017-07-26 17:11:00 +0800

    修改pid_set类中占空比与转速之间的函数关系

--- test/pid_realize.py
+++ test/pid_realize.py
@@ -3,7 +3,8 @@
 
 from __future__ import division
 import time
-import os,sys
+import os
+import sys
 
 
 #define PID controller
@@ -12,12 +13,12 @@ class pid_set(object):
 	def __init__(self,
 				SetSpeed = 0, ActualSpeed = 0, duty_cycle = 0,
 				err = 0, err_integrate = 0, err_tmp = 0, err_last = 0,
-				Kp = 0, Ki = 0, Kd = 0):
+				Kp = 1, Ki = 0.08, Kd = 2):
 		self.SetSpeed = SetSpeed                    #the expected speed
 		self.ActualSpeed = ActualSpeed              #the actual speed
 		self.duty_cycle = duty_cycle                #the mark_space ratio
 		self.err = err                              #the difference between SetSpeed and ActualSpeed
-		self.err_integrate = self.integrate         #the accumualte of err
+		self.err_integrate = err_integrate          #the accumualte of err
 		self.err_tmp = err_tmp                      #the error in t-1
 		self.err_last = err_last                    #the error in t-2
 		self.Kp = Kp                                #the proportion of pid controller
@@ -44,15 +45,19 @@ class pid_set(object):
 
 	def pid_realize(self):
 		self.err = self.SetSpeed - self.ActualSpeed
+		print("err:",self.err)
 		index = 0
 		if(abs(self.err) > self.SetSpeed):
 			pass
 		else:
 			index = 1
-			self.err_integrate += self.err_integrate
+			self.err_integrate += self.err
 		speed = self.Kp * self.err + index * self.Ki * self.err_integrate + self.Kd * (self.err - self.err_tmp)
+		print("speed:",speed)
 		self.err_tmp = self.err
-		self.duty_cycle = 8.95 * speed - 53.94                #the value of k should be confirmed through the relationship between duty_cycle and speed
+		self.duty_cycle = int((speed + 53.94) / 8.95)              #the value of k should be confirmed through the relationship between duty_cycle and speed
+		if self.duty_cycle >= 90:
+			self.duty_cycle = 90
 		return self.duty_cycle
 		
 			                    
@@ -70,11 +75,53 @@ if __name__ == "__main__":
 		file_path[y] = os.path.join(dir_path, y)
 
 	pid1 = pid_set()
-	pid1.get_SetSpeed(100)
+	pid1.get_SetSpeed(500)
+	speed = []
+	with open(file_path['command'], 'w') as command:
+		command.write('run-direct')
 	start = time.perf_counter()
-	while time.perf_counter() - start <= 5:
+	while time.perf_counter() - start <= 10:
 		with open(file_path['speed'], 'r') as f1:
-			pid1.get_ActualSpeed(f1.readline())
+			val = int(f1.readline())
+			pid1.get_ActualSpeed(val)
+			speed.append(val)
+		with open(file_path['duty_cycle_sp'], 'w') as f2:
+			f2.write(str(pid1.pid_realize()))
+
+	with open(file_path['command'], 'w') as f3:
+		f3.write('stop')
+	print(speed)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
